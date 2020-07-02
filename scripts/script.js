6c7921663d87e3247881a8a92cf269fd709095d7
p4nash <nash.riaz1995@gmail.com>
2020-07-02 19:46:35 +0100

    Added rest of site

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
 (function(){
-
+    autosize($('.message'));
 });
+var embed = new Twitch.Embed("twitch-embed", {
+        width: 640,
+        height: 360,
+        channel: "p4nash",
+        layout: "video",
+        autoplay: false,
+        // only needed if your site is also embedded on embed.example.com and othersite.example.com
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+
+      embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+        var player = embed.getPlayer();
+        // player.play();
+      });
